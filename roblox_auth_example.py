49fa9cca317c7558318b4a516c0fd641ac64c24e
logankaminer <88823830+logankaminer@users.noreply.github.com>
2023-04-14 09:43:43 -0400

    Update roblox_auth_example.py

--- roblox_auth_example.py
+++ roblox_auth_example.py
@@ -1,45 +1,39 @@
-import SAT
+import sat
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-class RobloxAuth():
+class RobloxAuthenticator():
 	def __init__(self):
 		self.gid = None
 		self.x_csrf_token = None
 
-	@SAT.hook(url='https://www.roblox.com/')
+	@sat.hook(url='https://www.roblox.com/')
 	def set_gid(self, response):
 		soup = BeautifulSoup(response.text, 'html.parser')
 
 		self.x_csrf_token = soup.find('meta', attrs={'name': 'csrf-token'}).get('data-token')
 		self.gid = response.cookies['GuestData'].split('UserID=')[1]
 
-	@SAT.hook(url='https://auth.roblox.com/v2/login')
-	def get_text(self, response):
-		if response.request.method != 'POST':
-			return
+	@sat.hook(url='https://auth.roblox.com/v2/login')
+	def get_auth_data(self, response):
+		if response.request.method == 'POST':
+			print(response.json())
 
-		auth_data = response.json()
-		if 'isBanned' in auth_data:
-			print('\n[*]Authentication Successful ...\n')
-		else:
-			print('\n[*]Authentication FAILED ...\n')
-
-config = SAT.Config(
-	har_file=r'example.har',
-	hook_class=RobloxAuth,
+config = sat.Config(
+	har_file='roblox_auth.har',
+	hook_class=RobloxAuthenticator,
 	payload_dict={
 		'cvalue': 'USERNAME',
 		'password': 'PASSWORD'
 	},
 	param_dict={
 		'lt': datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-4] + 'Z',
-		'gid': SAT.get_instance_attr('gid'),
+		'gid': sat.get_instance_attr('gid'),
 	},
 	headers_dict={
-		'x-csrf-token': SAT.get_instance_attr('x_csrf_token')
-	}
+		'x-csrf-token': sat.get_instance_attr('x_csrf_token')
+	},
 )
 
-instance = SAT.SATFramework(config)
+instance = sat.Framework(config)
 instance.main()
